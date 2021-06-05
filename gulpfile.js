4291662abc49d0f503bdc9f9e9c375942fd6f579
eparzych <emilia.parzych92@gmail.com>
2021-06-05 17:03:08 +0200

    2. nawigacja, header, sekcja

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ const browserSync = require("browser-sync").create();
 
 function compileSass(done) {
   gulp
-    .src(entryPath + "/scss/main.scss")
+    .src(entryPath + "/scss/style.scss")
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: "expanded"}).on("error", sass.logError))
     .pipe(autoprefixer())
